d01c3b14e3fdd9693c6f462c48d8a5d72269f99f
Artur Ciepielowski <artciepi@gmail.com>
2022-04-06 23:02:59 +0200

    Task completed

--- src/main/java/com/sda/arppl2/Main.java
+++ src/main/java/com/sda/arppl2/Main.java
@@ -4,6 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Zadanie 1 wykonane!");
 
+        Osoba firstPerson = new Osoba("Agnieszka", 165);
+        Osoba secondPerson = new Osoba("Gniewko", 186);
+        Osoba thirdPerson = new Osoba("Patrycjusz", 205);
+
+       // System.out.println(firstPerson.personName); //test
+        //System.out.println(secondPerson.personName); //test
+       // System.out.println(thirdPerson.personName); //test
     }
 
 }

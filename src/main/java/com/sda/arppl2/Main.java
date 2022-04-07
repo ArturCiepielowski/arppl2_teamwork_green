08c42315d462189072312ce2117809db295b22b9
alicj <alicja.mojska@gmail.com>
2022-04-07 22:24:10 +0200

    Dopisane kotki

--- src/main/java/com/sda/arppl2/Main.java
+++ src/main/java/com/sda/arppl2/Main.java
@@ -2,6 +2,10 @@ package com.sda.arppl2;
 
 public class Main {
     public static void main(String[] args) {
+
+        Kotek kotek1 = new Kotek("Zawisza", "Czarny");
+        Kotek kotek2 = new Kotek("Rydz", "Rudy");
+
         System.out.println("Zadanie 1 wykonane!");
     }
 

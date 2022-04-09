c9a525221ace747963acff5dd780a11d7ba48293
bKlamann92 <bartosz.klamann@gmail.com>
2022-04-09 16:22:23 +0200

    Piesek1

--- src/main/java/com/sda/arppl2/Main.java
+++ src/main/java/com/sda/arppl2/Main.java
@@ -3,6 +3,15 @@ package com.sda.arppl2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Zadanie 1 wykonane!");
+
+
+        Piesek piesek = new Piesek()
+
+
+
+
+
+
     }
 
 }
---
+++ src/main/java/com/sda/arppl2/Piesek.java
@@ -0,0 +1,15 @@
+package com.sda.arppl2;
+
+public class Piesek {
+
+        String imie;
+        int wiek;
+        int iloscLapek;
+
+    public Piesek(String imie, int wiek, int iloscLapek) {
+        this.imie = imie;
+        this.wiek = wiek;
+        this.iloscLapek = iloscLapek;
+    }
+}
+
